20be1b4a4ee76c88330aa3ae1b88df79e946d036
Timothy Smith <tsontario@gmail.com>
2020-01-25 21:48:36 -0500

    make directories actual pkgs

--- preprocess_courses.py
+++ preprocess_courses.py
@@ -4,7 +4,7 @@
 # Preventing overwrites will be done as part of the main component once
 # this implementation has proven correct
 
-import pkg.preprocessor.preprocessor as preprocessor
+from pkg.preprocessor import ottawa_university_preprocessor as preprocessor
 from os import path
 import logging
 
@@ -12,8 +12,8 @@ import logging
 infile_path = path.abspath(path.join("data", "raw", "UofO_Courses.html"))
 outfile_path = path.abspath(path.join("data", "corpus", "UofO_Courses.json"))
 
-preprocessor = preprocessor.ottawa_university_preprocessor(infile_path, outfile_path)
+runner = preprocessor(infile_path, outfile_path)
 
 print("Before")
-preprocessor.preprocess()
+runner.preprocess()
 print("Done")
