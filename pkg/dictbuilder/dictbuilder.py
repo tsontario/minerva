217896ab83f600367ea5e4efe8ca5bf35f0eece5
Timothy Smith <tsontario@gmail.com>
2020-01-26 23:17:33 -0500

    Fix some typos, lowercase, then sort the dictionary (might not be necessary/desirable

--- pkg/dictbuilder/dictbuilder.py
+++ pkg/dictbuilder/dictbuilder.py
@@ -4,7 +4,9 @@ import nltk
 
 
 class DictBuilder:
+    NLTK_STOPWORDS = nltk.corpus.stopwords.words("english")
     def __init__(self, corpus_handle, tokenizer=nltk.TweetTokenizer()):
+
         self.tokenizer = tokenizer
         self.corpus_handle = corpus_handle
         self.terms = set()
@@ -16,18 +18,20 @@ class DictBuilder:
                 "..",
                 "data",
                 "dictionary",
-                "UofOCourse.txt",
+                "UofOCourses.txt",
             )
         )
 
     # TODO: add optional parameters for enabling/disabling stopwords, stemming/lemmatization, and normalization
     def build(self):
-        corpus_stream = yaml.load_all(self.corpus_handle)
+        corpus_stream = yaml.load_all(self.corpus_handle, Loader=yaml.Loader)
         for doc in corpus_stream:
             self.terms = self.terms.union(self.tokenizer.tokenize(doc.course.contents))
 
         out = open(self.outfile_path, "w")
-        for term in sorted(self.terms):
-            out.write(term)
-            out.write("\n")
-        print(len(self.terms))
+        for term in sorted(map(lambda x : x.lower(), self.terms)):
+            term = term
+            if not term in type(self).NLTK_STOPWORDS:
+                out.write(term)
+                out.write("\n")
+        print(f"{len(self.terms)} unique terms written to {path.abspath(self.outfile_path)}")
