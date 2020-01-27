1d9ea9dff0596a001bc252f3f2ee9d8f0bda7e3a
Timothy Smith <tsontario@gmail.com>
2020-01-26 23:42:46 -0500

    Log stopwords processed if enable_stopwords is true

--- dict_build.py
+++ dict_build.py
@@ -2,5 +2,5 @@ from pkg.dictbuilder import dictbuilder
 
 corpus_handle = open("data/corpus/UofO_Courses.yaml", "r")
 
-dict_builder = dictbuilder.DictBuilder(corpus_handle)
+dict_builder = dictbuilder.DictBuilder(corpus_handle, enable_stopwords=True)
 dict_builder.build()
