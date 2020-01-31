3b16e46b03f8bbccd8cb5178d735660817e6ea0e
Timothy Smith <tsontario@gmail.com>
2020-01-30 23:20:00 -0500

    reminder to myself about how to implement the inverted index

--- pkg/indexbuilder/ottawau.py
+++ pkg/indexbuilder/ottawau.py
@@ -19,6 +19,13 @@ class OttawaUIndexBuilder(IndexBuilder):
         self.dict_handle = open(self.dict_path, 'r')
         self.inverted_index_handle = open(self.inverted_index_path, 'w')
 
+# TODO: So here's what we need to do:
+# STEP 1:
+#   - FOR EACH DOCUMENT, read the doc, call normalizers/filters, etc. and then generate a TERM -> DOC_ID mapping. DOC_ID should ALSO HAVE NUMBER OF TIMES TERM OCCURS
+#   - Once this is done for all documents, alphabetize the list on TERM: identical terms are now together
+#   - For a given term: WHILE the current term consider is equal, increase DOC_FREQ by 1 and maintain a list of the DOC_ID (objects!) that make up the list for the terms
+#   - The result of these operations is your inverted index. Congratulations
+
         # corpus_stream = yaml.load_all(self.corpus_handle, Loader=yaml.Loader)
         # if os.getenv("DEBUG"):
         #     print("Building inverted index (uOttawa)...")
