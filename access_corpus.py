976ada68bbd858ce75847f46dad237c4974b7ea1
aethelind <aethelind@gmail.com>
2020-02-03 15:08:42 -0500

    convert accessor to singleton

--- access_corpus.py
+++ access_corpus.py
@@ -10,9 +10,19 @@ ctx = Context(corpus_path, "", "")
 print("Initializing CorpusAccessor")
 corpus_accessor = CorpusAccessor(ctx)
 
-print("Now accessing docs:")
+print("Accessing docs:")
 
-docs = corpus_accessor.access([587,588,589])
+docs = corpus_accessor.access(ctx, [587,588,589])
+
+for d in docs:
+	print(d)
+
+print("\nTry initializing again with same corpus (doesn't re-load)")
+corpus_accessor = CorpusAccessor(ctx)
+
+print("Accessing docs:")
+
+docs = corpus_accessor.access(ctx, [590,591,592])
 
 for d in docs:
 	print(d)
--- pkg/corpusaccess/corpusaccessor.py
+++ pkg/corpusaccess/corpusaccessor.py
@@ -1,26 +1,41 @@
 import yaml
 
-# TODO: deal with changing corpus, ie. from uOttawa to Reuters 
+
 class CorpusAccessor:
+	# private construction for singleton
+	class __CorpusAccessor:
+		def __init__(self, ctx):
+			self.ctx = ctx
+			self.documents = {}
+
+			with open(self.ctx.corpus_path, "r") as corpus_handle:
+				corpus_stream = yaml.load_all(corpus_handle, Loader=yaml.Loader)
+				for doc in corpus_stream:
+					self.documents[doc.id] = doc
+	
+	corpora = {}
 	def __init__(self, ctx):
-		self.ctx = ctx
-		self.documents = {}
-
-		with open(self.ctx.corpus_path, "r") as corpus_handle:
-			corpus_stream = yaml.load_all(corpus_handle, Loader=yaml.Loader)
-			for doc in corpus_stream:
-				self.documents[doc.id] = doc
-
-	def access(self, doc_ids):
+		# if the corpus is not yet in accessor, then load the corpus and put it in corpora
+		if not ctx.corpus_path in self.corpora:
+			CorpusAccessor.corpora[ctx.corpus_path] = CorpusAccessor.__CorpusAccessor(ctx)
+			# is using the corpus_path as a dict key a bad idea?
+		else:
+			print("Corpus " + ctx.corpus_path + " already loaded.")
+	
+	def access(self, ctx, doc_ids):
 		if not doc_ids:
 			print("No document IDs given.")
 			return []
+		elif not ctx.corpus_path in self.corpora:
+			print("Corpus not loaded into memory.")
+			return []
 
+		accessor = self.corpora[ctx.corpus_path]
 		results = []
 
 		for i in doc_ids:
-			if i in self.documents:
-				results.append(self.documents[i])
+			if i in accessor.documents:
+				results.append(accessor.documents[i])
 			else:
 				print("Invalid document id given: " + str(i))
 
