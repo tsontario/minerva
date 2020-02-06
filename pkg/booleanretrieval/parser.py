71f737f9283a76ddb0a062613fdabb176473d07f
Timothy Smith <tsontario@gmail.com>
2020-02-05 22:18:44 -0500

    start of boolean retrieval: query processing

--- pkg/booleanretrieval/parser.py
+++ pkg/booleanretrieval/parser.py
@@ -1,9 +1,10 @@
 import re
 
+
 class Stack:
     def __init__(self):
         self._stack = []
-    
+
     def push(self, val):
         self._stack.append(val)
         return val
@@ -12,6 +13,7 @@ class Stack:
         if len(self._stack) == 0:
             return None
         return self._stack.pop()
+
 
 # Example: (computer AND thing)
 class Parser:
@@ -37,14 +39,15 @@ class Parser:
                 i += 1
             elif re.match(r"\s", expr[i]):  # whitespace
                 i += 1
-            else: # an operator or operand
+            else:  # an operator or operand
                 word = ""
-                while i < len(expr) and re.match(r"\s|\(|\)", expr[i]) is None: # Not whitespace and not a parens
+                while (
+                    i < len(expr) and re.match(r"\s|\(|\)", expr[i]) is None
+                ):  # Not whitespace and not a parens
                     word += expr[i]
                     i += 1
                 result.append(word)
         return result
-                
 
     # SOURCE: https://www.includehelp.com/c/infix-to-postfix-conversion-using-stack-with-c-program.aspx
     def _to_postfix(self, expr):
@@ -70,18 +73,22 @@ class Parser:
                     if popped == "(":
                         break
                     result.append(c)
-            else: # This must be an operator
+            else:  # This must be an operator
                 result.append(c)
         return result
+
 
 def is_operator(token):
     return token in ["AND", "OR", "NOT"]
 
+
 def is_left_parens(c):
     return c == "("
 
+
 def is_right_parens(c):
     return c == ")"
+
 
 def is_parens(c):
     return is_right_parens(c) or is_left_parens(c)
