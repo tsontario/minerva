349f8660088dbafbcf7b994d58663572057a0147
Timothy Smith <tsontario@gmail.com>
2020-02-08 11:06:36 -0500

    Apply same set of filters/normalizers to query as to current dict/index

--- parse.py
+++ parse.py
@@ -1,6 +1,10 @@
 from os import path
 
+<<<<<<< HEAD
 from pkg.booleanretrieval import Parser, Evaluator
+=======
+from pkg.booleanretrieval import parser
+>>>>>>> Apply same set of filters/normalizers to query as to current dict/index
 from pkg.context import Context
 
 corpus_path = "data/corpus/UofO_Courses.yaml"
@@ -8,7 +12,12 @@ dictionary_path = "data/dictionary/UofOCourses.txt"
 index_path = path.realpath("data/index/UofO_Courses.yaml")
 ctx = Context(corpus_path, dictionary_path, index_path)
 
+<<<<<<< HEAD
 p = Parser(ctx)
 parsed = p.parse("(learn AND (leading OR particular) ) OR material")
 doc_ids = Evaluator(ctx, parsed).evaluate()
 print(doc_ids)
+=======
+p = parser.Parser(ctx)
+p.parse("word OR a AND (another OR alternate)")
+>>>>>>> Apply same set of filters/normalizers to query as to current dict/index
