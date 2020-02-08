03297d8880efdc0ba777177ef7180a33c1c4b362
Timothy Smith <tsontario@gmail.com>
2020-02-08 13:34:29 -0500

    stash

--- pkg/booleanretrieval/evalutator.py
+++ pkg/booleanretrieval/evalutator.py
@@ -1,2 +1,15 @@
+
+# Evaluator objects evaluate boolean expressions and return the document IDs associated with the 
+# evaluated expression
 class Evaluator:
-    pass
+    def __init__(self, expr):
+        self.expr = expr
+
+    def evaluate(self):
+        self._convert_to_doc_ids
+
+    # converts the operands of self.expr into arrays of doc ids
+    def _convert_to_doc_ids(self):
+        for i, token in enumerate(self.expr):
+            if is_operand(token):
+                
