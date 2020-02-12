317db302b4817e19d254fba15c35b9058a1d286b
Timothy Smith <tsontario@gmail.com>
2020-02-11 20:14:39 -0500

    dictbuilder uses digest

--- pkg/dictionary/dictbuilder.py
+++ pkg/dictionary/dictbuilder.py
@@ -12,14 +12,12 @@ class DictBuilder:
     def __init__(self, ctx):
         self.ctx = ctx
         self.tokenizer = ctx.tokenizer
-        self.corpus_path = ctx.corpus_path
-        self.outfile_path = ctx.dict_path
         self.normalize_funcs = context.normalizer_funcs_for_context(ctx)
         self.filter_funcs = context.filter_funcs_for_context(ctx)
 
     def build(self):
         terms = set()
-        with open(self.ctx.corpus_path, "r") as corpus_handle:
+        with open(self.ctx.corpus_path(), "r") as corpus_handle:
             corpus_stream = yaml.load_all(corpus_handle, Loader=yaml.Loader)
             if os.getenv("DEBUG"):
                 print("Performing configured normalizations and tokenization...")
@@ -30,8 +28,8 @@ class DictBuilder:
                 terms = terms.union(self.tokenizer.tokenize(contents))
         for filter_func in self.filter_funcs:
             terms = filter_func(terms.copy())
-        with open(self.outfile_path, "w") as outfile:
+        with open(self.ctx.dict_path(), "w") as outfile:
             for term in sorted(terms):
                 outfile.write(term)
                 outfile.write("\n")
-        print(f"{len(terms)} unique terms written to {path.abspath(self.outfile_path)}")
+        print(f"{len(terms)} unique terms written to {path.abspath(self.ctx.dict_path())}")
