a39fdb019a41104c539a65bb07d12c109945bef0
Tim <tsontario@users.noreply.github.com>
2020-04-10 10:30:34 -0400

    K-NN Module (#26)
    * scaffold knn categorization class
    
    * starting to get the VSM query stuff going. Some TODO to play nice between corpora
    
    * call correct index creators depending on corpus type (reuters or uottawa)
    
    * basic topic assignment works'
    
    * topic assignment algo working

--- pkg/context/context.py
+++ pkg/context/context.py
@@ -64,6 +64,12 @@ class Context:
             + f"_weighted_index_{self._digest()}.yaml"
         )
 
+    def corpus_type(self):
+        if self._corpus_path.endswith("reuters.yaml"):
+            return "reuters"
+        else:
+            return "ottawa_u"
+
     # digest returns the hex digest of the current context object in regards to those elements
     # configurable by the user. This digest can be (indeed, is) appended to the end of generated files
     # such as dictionaries and indices, so that we can maintain persistent records without having
