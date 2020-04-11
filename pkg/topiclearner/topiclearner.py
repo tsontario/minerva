4ad9568c2caf9f24d814542498823b440f7a28be
Timothy Smith <tsontario@gmail.com>
2020-04-11 16:49:27 -0400

    tidy

--- pkg/topiclearner/topiclearner.py
+++ pkg/topiclearner/topiclearner.py
@@ -17,13 +17,6 @@ from pkg.corpusaccess import CorpusAccessor
 
 
 class TopicLearner:
-    # strategy:
-    # For each document: take all words (uniq) and use as a VSM query. Then take the top N ranked documents and take their topics.
-    # If a topic doesn't exist on the doc.... keep going until you find one.
-
-    # Architecture:
-    # We can simply add more topics and rewrite out the corpus with the new values (let's write it to somewhere different, mind you)
-    # So... one static method: learn(ctx)
     def __init__(self, ctx, k=5):
         self.ctx = ctx
         self.corpus_accessor = CorpusAccessor(ctx)
