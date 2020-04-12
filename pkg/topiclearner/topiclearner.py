d7169b7e30aed271a9fa9e5cc3950a56ce529b84
Tim <tsontario@users.noreply.github.com>
2020-04-12 10:15:44 -0400

    Knn categorization (#28)
    * Ran topic assigner
    
    * tidy

--- pkg/topiclearner/topiclearner.py
+++ pkg/topiclearner/topiclearner.py
@@ -5,8 +5,11 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
+import sys
+import os
 import time
 from itertools import chain
+from more_itertools import divide
 from collections import Counter
 
 from ..vsm import VectorSpaceModel
@@ -14,13 +17,6 @@ from pkg.corpusaccess import CorpusAccessor
 
 
 class TopicLearner:
-    # strategy:
-    # For each document: take all words (uniq) and use as a VSM query. Then take the top N ranked documents and take their topics.
-    # If a topic doesn't exist on the doc.... keep going until you find one.
-
-    # Architecture:
-    # We can simply add more topics and rewrite out the corpus with the new values (let's write it to somewhere different, mind you)
-    # So... one static method: learn(ctx)
     def __init__(self, ctx, k=5):
         self.ctx = ctx
         self.corpus_accessor = CorpusAccessor(ctx)
@@ -31,7 +27,6 @@ class TopicLearner:
     def learn(self):
         self._partition_articles()
         self._classify()
-        self._write()
 
     # partition documents between those that have topics (TRAINING SET) and those that don't
     def _partition_articles(self):
@@ -48,13 +43,42 @@ class TopicLearner:
 
     # Using the training set, classify documents with missing topics
     def _classify(self):
+        partitions = [list(d) for d in divide(100, self.unclassified_set)]
         print("loading VSM")
         start = time.time()
         vsm = VectorSpaceModel(self.ctx)
         end = time.time()
         print(f"loading VSM took {end - start} seconds")
-        for article in self.unclassified_set:
-            start = time.time()
+        for p in partitions:
+            self._run(p, vsm)
+        # Now add the training set, we now have a full corpus with topics assigned
+        with open(f"{self.ctx.corpus_path()}_topics.yaml", "a") as outfile:
+            dump_all(
+                self.training_set,
+                outfile,
+                explicit_start=True,
+                default_flow_style=False,
+                sort_keys=False,
+                indent=2,
+                Dumper=Dumper,
+            )
+
+    # unfortunately, doing this all in one process causes memory use to grow without bound and eventually the process gets OOMkilled.
+    # I made some attempts to reason about where the memory growth is occurring, and it's most likely due to the size of the
+    # generated output. However, even partitioning into smaller runs (and appending to the output as needed), the OOMkill was still occurring.
+    # In the end, I opted to simply fork each run so that memory is cleaned up when the child process dies. It's quite hacky, and only works
+    # on UNIX systems, so this code won't run properly on Windows.
+    def _run(self, p, vsm):
+        i = 1
+        pid = os.fork()
+        if pid != 0:
+            os.waitpid(0, 0)
+            print(f"Child process is finished")
+            return
+        # otherwise we are in child process
+        for article in p:
+            print(f"{i}/{len(self.unclassified_set)}")
+            i += 1
             results = vsm.search(self.ctx, article.read_queryable())
             documents = self.corpus_accessor.access(self.ctx, [r[0] for r in results])
             documents = [d for d in documents if len(d.topics) > 0]
@@ -66,12 +90,9 @@ class TopicLearner:
                 continue
             self._assign_topics(article, documents)
             print(f"New topics for {article.title}: {article.topics}")
-            end = time.time()
-            print(f"Time elapsed for {article.title}: {end - start} seconds")
-        full_set = self.unclassified_set + self.training_set
-        with open(self.ctx.corpus_path(), "w") as outfile:
+        with open(f"{self.ctx.corpus_path()}_topics.yaml", "a") as outfile:
             dump_all(
-                full_set,
+                p,
                 outfile,
                 explicit_start=True,
                 default_flow_style=False,
@@ -79,9 +100,8 @@ class TopicLearner:
                 indent=2,
                 Dumper=Dumper,
             )
-
-    def _write(self):
-        print("STUB")
+            print("WROTE")
+        sys.exit()
 
     # Given a set of documents that are the nearest neightbours of article, extract the relevant topics
     # Strategy: from N nearest negihbours, assign topics that appear in the majority
